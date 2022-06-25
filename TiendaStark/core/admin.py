7043792e788964670963436aea27a85c2f9093bb
Baespinosav <ba.espinosav@duocuc.cl>
2022-06-25 19:46:39 -0400

    uwu

---
+++ TiendaStark/core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Categoria, Usuario, Vehiculo, PerfilUsuario, categoriaProducto, manBod, manProducto
+
+# Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Vehiculo)
+admin.site.register(PerfilUsuario)
+admin.site.register(categoriaProducto)
+admin.site.register(manProducto)
+admin.site.register(Usuario)
+admin.site.register(manBod)
